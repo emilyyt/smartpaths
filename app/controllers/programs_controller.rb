afd7f6a2bd33a487408b391698233e9d06a39c3e
Emily Tsai <cetsai@andrew.cmu.edu>
2013-11-14 21:49:41 -0500

    fixed success notice bug

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -47,8 +47,8 @@ class ProgramsController < ApplicationController
 
     respond_to do |format|
       if @program.save
-        format.html { redirect_to @program, notice: 'Program was successfully created.' }
-        format.json { head :no_content }
+        format.html { redirect_to @program, notice: "Program was successfully created." }
+        format.json { render json: @program, status: :created, location: @program }
       else
         format.html { render action: "new" }
         format.json { render json: @program.errors, status: :unprocessable_entity }
