868f9e2c86f5ea08a014594cc273ffb2e500b775
studentinc <nessumaza@hotmail.com>
2013-12-02 19:35:37 -0500

    Weird minor double table issue on institution profiles fixed

--- db/schema.rb
+++ db/schema.rb
@@ -117,6 +117,16 @@ ActiveRecord::Schema.define(:version => 20131121211057056) do
   add_index "rs_reputations", ["reputation_name"], :name => "index_rs_reputations_on_reputation_name"
   add_index "rs_reputations", ["target_id", "target_type"], :name => "index_rs_reputations_on_target_id_and_target_type"
 
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
   create_table "tags", :force => true do |t|
     t.string   "name"
     t.string   "tag_type"
